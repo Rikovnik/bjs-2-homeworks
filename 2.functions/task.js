75b450a6f1e9c8dade4cb881a383735521a86964
Rikov_Nik <rikovnn@gmail.com>
2022-07-24 00:11:26 +0500

    HW2_functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,72 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+
+    sum = sum + arr[i];
+  }
+
+  avg = Number((sum / arr.length).toFixed(2));  
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
+  for (j = 0; j < arrOfArr.length; j++) {
+    
+    if (max < func(arrOfArr[j])) {
+      max = func(arrOfArr[j]);
+    } 
+  }
   
   return max;
 }
 
+
+
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  let dif;
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    if (arr[i] > max) {
+      max = arr[i];
+    }    
+  }
+
+  dif = Math.abs(max - min);
+
+  return dif;
 }
