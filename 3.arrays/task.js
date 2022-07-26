68d47db1d5a27132ea1132462d4bce288ad0cc03
Rikov_Nik <rikovnn@gmail.com>
2022-07-26 23:26:12 +0500

    HW_3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,21 @@
-function compareArrays(arr1, arr2) {
-  let result;
+  function compareArrays(arr1, arr2) {
 
-  // Ваш код
+    let result 
 
-  return result; // boolean
+    const comare = arr1.every((item,i) => item === arr2[i]);
+
+    if (arr1.length === arr2.length && comare === true) {
+      result = true;}
+    else {
+      result = false}
+
+    return result;
 }
 
-function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+function advancedFilter(arr) {
+  
+  let resultArr = arr.filter((number) => number > 0).filter((number) => number % 3 === 0).map((number) => number * 10);
 
   return resultArr; // array
 }
