a7feba80043b271fb7a1933795d3af95abcb1234
Rikov_Nik <rikovnn@gmail.com>
2022-07-31 11:01:56 +0500

    HW_4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,40 @@
 function Student(name, gender, age) {
-    // Ваш код
-
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+};
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark]
+    } else {
+      this.marks.push(mark)
+    }
+};
+
+Student.prototype.addMarks = function (... mark) {
+  if(this.marks === undefined){ 
+    this.marks = [... mark];
+    } else {
+      this.marks.push(...mark)
+    }
+};
+
+Student.prototype.getAverage = function (marks) {
+  let sum = 0;
+  for(let i in this.marks) {
+  sum = sum + this.marks[i]
+  }
+  
+  return getAverage = sum / this.marks.length;
+};
+
+Student.prototype.exclude = function (reason) {
+  this.excluded  = reason;
+  delete this.marks;
+  delete this.subject;
+};
