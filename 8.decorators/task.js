19321c9d6d06f969c9ed74a2940d013e8ce611db
Rikov_Nik <rikovnn@gmail.com>
2022-10-14 22:58:15 +0500

    Доработка ДЗ

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -4,76 +4,46 @@ function cachingDecoratorNew(func) {
   function wrapper(...args) {
     const hash = args.join(',');
     let objectInCache = cache.find((item) => item.hash == hash);
-    if (!objectInCache) {
-      let result = func(...args);
+    if (objectInCache) {
+      console.log("Из кэша: " + cache[cache.indexOf(objectInCache)].value);
+      return "Из кэша: " + cache[cache.indexOf(objectInCache)].value;
+    };
+
+    let result = func(...args);
       cache.push({
         hash: hash,
         value: result});
       if (cache.length > 5) {
         cache.shift();
-      }
+      };
 
       console.log("Вычисляем: " + result);
       return "Вычисляем: " + result;
-    } else {
-      console.log("Из кэша: " + cache[cache.indexOf(objectInCache)].value);
-      return "Из кэша: " + cache[cache.indexOf(objectInCache)].value;
-    }
   }
   return wrapper;
-}
+};
 
 
 
 
 function debounceDecoratorNew(func) {
+  let timerId;
+  let firstRunFlag = true;
 
-  const decor = (func, ms) => {
-    let firstRunFlag = true;
-
-    return function (...args) {
-      if(firstRunFlag) {
-        func.apply(this. args);
-        firstRunFlag = false;
-        return;
-      }
+  function wrapper(...args) {
+    if (firstRunFlag) {
+      wrapper.allCount = 1;
+      wrapper.count = 1;
+      firstRunFlag = false;
+      f.apply(this, args)
+      return ;
     }
+
+    clearTimeout(timerID);
+    wrapper.allCount += 1;
+    timerID = setTimeout( () => {
+        f.apply(this, args);
+        wrapper.count += 1;
+    }, ms);
   }
-
-  const debounceDecorator = (f, ms) => {
-    let timerId;
-
-    return function (...args) {
-      clearTimeout(timerId);
-
-      timerId = setTimeout(() => {
-        f.apply(this.args);
-        console.timeEnd("time");
-      }, ms);
-    };
-  };
 };
-
-
-
-function debounceDecorator2(func) {
-  const decor = (func, ms) => {
-
-    function wrapper(...args) {
-      this.count++;
-
-      let timerId;
-      return function (...args) {
-        clearTimeout(timerId);
-
-        timerId = setTimeout(() => {
-          f.apply(this.args);
-          console.timeEnd("time");
-        }, ms);
-      };
-    };
-
-    wrapper.count = 0;
-    return wrapper;
-  }
-}
