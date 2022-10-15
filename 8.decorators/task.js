0455e9c207a529646b626bbc531a528e053a402e
Rikov_Nik <rikovnn@gmail.com>
2022-10-16 00:13:20 +0500

    Доработка_2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -5,8 +5,8 @@ function cachingDecoratorNew(func) {
     const hash = args.join(',');
     let objectInCache = cache.find((item) => item.hash == hash);
     if (objectInCache) {
-      console.log("Из кэша: " + cache[cache.indexOf(objectInCache)].value);
-      return "Из кэша: " + cache[cache.indexOf(objectInCache)].value;
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
     };
 
     let result = func(...args);
@@ -35,14 +35,14 @@ function debounceDecoratorNew(func) {
       wrapper.allCount = 1;
       wrapper.count = 1;
       firstRunFlag = false;
-      f.apply(this, args)
+      func.apply(this, args)
       return ;
     }
 
     clearTimeout(timerID);
     wrapper.allCount += 1;
     timerID = setTimeout( () => {
-        f.apply(this, args);
+        func.apply(this, args);
         wrapper.count += 1;
     }, ms);
   }
